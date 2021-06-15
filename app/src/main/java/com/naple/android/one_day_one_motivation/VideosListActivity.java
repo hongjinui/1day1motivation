1c1166f464c58a325002866bcf1704caf0019e02
hongjinui <36667004+hongjinui@users.noreply.github.com>
2021-06-15 16:24:52 +0900

    20210615 toolbar/natigation

--- app/src/main/java/com/naple/android/one_day_one_motivation/VideosListActivity.java
+++ app/src/main/java/com/naple/android/one_day_one_motivation/VideosListActivity.java
@@ -1,14 +1,73 @@
  package com.naple.android.one_day_one_motivation;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class VideosListActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+ public class VideosListActivity extends AppCompatActivity {
+
+     private Toolbar toolbar;
+     private DrawerLayout drawerLayout;
+     private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos_list);
+
+        navigationView = findViewById(R.id.NavigationView);
+        drawerLayout = findViewById(R.id.DrawerLayout);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.menu);
+
+
+        // 네비게이션 화면 이벤트 처리
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.exercise_motivation:
+                        Toast.makeText(VideosListActivity.this, "운동동기부여!", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
     }
-}
+
+
+    // 툴바에 메뉴들 나열?
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+         return true;
+     }
+
+     // 메뉴 아이템 선택 이벤트
+     @Override
+     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case android.R.id.home:
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+
+
+         return true;
+     }
+ }
